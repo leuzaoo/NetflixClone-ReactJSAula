9286ac8380a6b41217c417a757221db671396b1d
leuzaoo <leuzaoocontato@gmail.com>
2022-06-14 12:44:31 -0300

    MovieRow component added

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+// gallery
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+// components
+import Tmdb from "./Tmdb";
+import MovieRow from "./components/MovieRow";
+
+function App() {
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+    };
+
+    loadAll();
+  });
+
+  return (
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} />
+        ))}
+      </section>
+    </div>
+  );
+}
+
+export default App;
