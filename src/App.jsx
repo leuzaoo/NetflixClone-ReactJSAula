33315dd6aa158c76445ca08ac9983b80234e5702
leuzaoo <leuzaoocontato@gmail.com>
2022-06-15 10:47:08 -0300

    movie or serie in homepage

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,30 @@ import "./App.css";
 // components
 import Tmdb from "./Tmdb";
 import MovieRow from "./components/MovieRow";
+import FeaturedMovie from "./components/FeaturedMovie";
 
 function App() {
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
 
   useEffectOnce(async () => {
     let list = await Tmdb.getHomeList();
-    console.log(list);
     setMovieList(list);
+
+    let originals = list.filter((i) => i.slug === "originals");
+    let randomChosen = Math.floor(
+      Math.random() * (originals[0].items.results.length - 1)
+    );
+    let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv')
+      console.log(chosenInfo)
+
   });
 
   return (
     <div className="page">
+      {featuredData && <FeaturedMovie item={featuredData} />}
+
       <section className="lists">
         {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} />
