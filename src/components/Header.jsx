5876e93a5190351d8b8eeffca7974c1249caae8a
leuzaoo <leuzaoocontato@gmail.com>
2022-06-15 16:39:13 -0300

    header finished

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import './Header.css'
+import React from "react";
+import "./Header.css";
 
 const Header = () => {
-    return ( 
+  return (
+    <header>
+      <div className="header--logo">
+        <a href="/">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+            alt=""
+          />
+        </a>
+      </div>
+      <div className="header--user">
+        <a href="">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="" />
+        </a>
+      </div>
+    </header>
+  );
+};
 
-        <header>
-
-
-
-
-
-        </header>
-
-
-     );
-}
- 
 export default Header;
