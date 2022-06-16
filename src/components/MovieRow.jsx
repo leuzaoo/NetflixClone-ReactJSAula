bb2aa3f3e850f34d5896876d333c2f83b64ce2cd
leuzaoo <leuzaoocontato@gmail.com>
2022-06-16 12:17:57 -0300

    leftArrow scroll function

--- src/components/MovieRow.jsx
+++ src/components/MovieRow.jsx
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MovieRow.css";
+import { MdNavigateBefore } from "react-icons/md";
+import { MdNavigateNext } from "react-icons/md";
 
 export default ({ title, items }) => {
+  const [scrollX, setScrollX] = useState(-400);
+
+  const handleLeftArrow = () => {
+    let x = scrollX + 150;
+    if (x > 0) {
+      x = 0;
+    }
+
+    setScrollX(x);
+  };
+
+  const handleRightArrow = () => {};
+
   return (
     <div className="movie-row">
       <h2>{title}</h2>
-      <div className="movieRow--listarea">
+
+      <div className="movieRow--left" onClick={handleLeftArrow}>
+        <MdNavigateBefore style={{ fontSize: 50 }} />
+      </div>
+      <div className="movieRow--right" onClick={handleRightArrow}>
+        <MdNavigateNext style={{ fontSize: 50 }} />
+      </div>
+      <div
+        className="movieRow--listarea"
+        style={{
+          marginLeft: scrollX,
+          width: items.results.length * 225,
+        }}
+      >
         <div className="movieRow--list">
           {items.results.length > 0 &&
             items.results.map((item, key) => (
