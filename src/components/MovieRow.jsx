54a62155ee3e1e35021671c2347dc43dc4a98242
leuzaoo <leuzaoocontato@gmail.com>
2022-06-16 12:26:50 -0300

    function right arrow scroll with user screen

--- src/components/MovieRow.jsx
+++ src/components/MovieRow.jsx
@@ -16,9 +16,12 @@ export default ({ title, items }) => {
   };
 
   const handleRightArrow = () => {
-
-
-    
+    let x = scrollX - Math.round(window.innerWidth / 2);
+    let listW = items.results.length * 150;
+    if((window.innerWidth - listW) > x) {
+      x = (window.innerWidth - listW) - 60;
+    }
+    setScrollX(x);
   };
 
   return (
